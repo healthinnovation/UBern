a281e2de1b6ec6ebf8edd67eb74ef01c0868c31c
Antony Barja <antony.barja8@gmail.com>
2022-10-16 21:14:35 -0500

    Update 00_get_data.R

--- script/00_get_data.R
+++ script/00_get_data.R
@@ -8,44 +8,8 @@ download.file(
   method = 'curl'
 )
 
-defunciones <- vroom("data/defunciones.csv",
-  col_types = cols(
-    Nº = col_integer(),
-    `TIPO SEGURO` = col_factor(),
-    SEXO = col_factor(),
-    EDAD = col_integer(),
-    `TIEMPO EDAD` = col_character(),
-    `ESTADO CIVIL` = col_factor(),
-    `NIVEL DE INSTRUCCIÓN` = col_factor(),
-    ETNIA = col_factor() , 
-    `COD# UBIGEO DOMICILIO` = col_character(), 
-    `PAIS DOMICILIO` = col_character(),
-    `DEPARTAMENTO DOMICILIO` = col_factor(),
-    `PROVINCIA DOMICILIO` = col_factor(),
-    `DISTRITO DOMICILIO` = col_character(), 
-    FECHA = col_date(format = "%Y-%m-%d"),
-    AÑO = col_integer(),
-    MES = col_integer(),
-    `TIPO LUGAR` = col_character(),
-    INSTITUCION = col_character(),
-    `MUERTE VIOLENTA` = col_character(),
-    NECROPSIA = col_factor(),
-    `DEBIDO A (CAUSA A)` = col_character(),
-    `CAUSA A (CIE-X)`  = col_character(),
-    `DEBIDO A (CAUSA B)` = col_character(), 
-    `CAUSA B (CIE-X)` = col_character(), 
-    `DEBIDO A (CAUSA C)` = col_character(), 
-    `CAUSA C (CIE-X)`  = col_character(), 
-    `DEBIDO A (CAUSA D)` = col_character(), 
-    `CAUSA D (CIE-X)` = col_character(), 
-    `DEBIDO A (CAUSA E)` = col_character(),
-    `CAUSA E (CIE-X)` = col_character(),
-    `DEBIDO A (CAUSA F)` = col_character(), 
-    `CAUSA F (CIE-X)` = col_character(),
-  )
-)
-
-names(defunciones) <- c(
+defunciones <- vroom("data/defunciones.csv")
+names_defunciones <- c(
   "ID","TIPO_SEGURO","SEXO","EDAD","TIEMPO_EDAD",
   "ESTADO_CIVIL","NIVEL_DE_INSTRUCCION","ETNIA",
   "UBIGEO","PAIS","DEPARTAMENTO","PROVINCIA",
@@ -56,7 +20,7 @@ names(defunciones) <- c(
   "DEBIDO_A.CAUSA_D","CAUSA_D.CIE_X","DEBIDO_A.CAUSA_E",
   "CAUSA_E.CIE_X","DEBIDO_A.CAUSA_F","CAUSA_F.CIE_X"
   )
-
+names(defunciones) <- names_defunciones 
 study_area <- c(
     "MADRE DE DIOS ","UCAYALI","JUNIN","HUANCAVELICA",
     "ICA","AYACUCHO","APURIMAC","CUSCO","PASCO","AREQUIPA",
